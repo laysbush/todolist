b4074a6f32768aadb3e4a9fac86b3037bc0d19b0
dimak <dimak@DMITRY>
2025-05-30 02:40:23 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,122 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TreeComponent, TreeNode} from './components/tree/tree.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TreeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
-export class AppComponent {
-  title = 'tree-view-app';
+export class AppComponent implements OnInit{
+  public treeNodes: TreeNode[] = [
+    {
+      "id": 1,
+      "title": "Значение 1",
+      "is_deleted": false,
+      "children": [
+        {
+          "id": 2,
+          "title": "Значение 1.1",
+          "is_deleted": false,
+          "children": [
+            {
+              "id": 3,
+              "title": "Значение 1.1.1",
+              "is_deleted": true,
+              "children": []
+            }
+          ]
+        },
+        {
+          "id": 4,
+          "title": "Значение 1.2",
+          "is_deleted": false,
+          "children": [
+            {
+              "id": 5,
+              "title": "Значение 1.2.1",
+              "is_deleted": false,
+              "children": []
+            },
+            {
+              "id": 6,
+              "title": "Значение 1.2.2",
+              "is_deleted": false,
+              "children": []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "title": "Значение 2",
+      "is_deleted": false,
+      "children": [
+        {
+          "id": 8,
+          "title": "Значение 2.1",
+          "is_deleted": true,
+          "children": [
+            {
+              "id": 9,
+              "title": "Значение 2.1.1",
+              "is_deleted": true,
+              "children": [
+                {
+                  "id": 10,
+                  "title": "Значение 2.1.1.1",
+                  "is_deleted": true,
+                  "children": [
+                    {
+                      "id": 11,
+                      "title": "Значение 2.1.1.1.1",
+                      "is_deleted": true,
+                      "children": [
+                        {
+                          "id": 12,
+                          "title": "Значение 2.1.1.1.1.1",
+                          "is_deleted": false,
+                          "deleted_at": null,
+                          "children": [
+                            {
+                              "id": 13,
+                              "title": "Значение 2.1.1.1.1.1.1",
+                              "is_deleted": false,
+                              "children": []
+                            }
+                          ]
+                        },
+                        {
+                          "id": 13,
+                          "title": "Значение 2.1.1.1.1.2",
+                          "is_deleted": false,
+                          "children": []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }
+  ]
+  private initSignals(nodes: TreeNode[]): void {
+    for (const node of nodes) {
+      if (!node.isOpen) {
+        node.isOpen = signal(false);
+      }
+      if (node.children?.length) {
+        this.initSignals(node.children);
+      }
+    }
+  }
+  public ngOnInit(): void {
+    this.initSignals(this.treeNodes);
+  }
 }
